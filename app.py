c9434caef4682fa9bb836880f3cee38dc84061e1
Zacharia Ria Ndungu <zachariarian@gmail.com>
2023-04-18 22:56:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,134 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+class Loan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    amount = db.Column(db.Integer)
+    date = db.Column(db.DateTime)
 
 @app.route('/')
-
-
-class Loan:
-    def __init__(self, amount, interest_rate, term):
-        self.amount = amount
-        self.interest_rate = interest_rate
-        self.term = term
-        self.payments = []
-
-def monthly_payment(self):
-    r = self.interest_rate / 100 / 12
-    n = self.term * 12
-    return self.amount * r * (1 + r) ** n / ((1 + r) ** n - 1)
-
-def payment_schedule(self):
-    payment = self.monthly_payment()
-    balance = self.amount
-    for i in range(self.term * 12):
-        interest = balance * self.interest_rate / 100 / 12
-        principal = payment - interest
-        balance -= principal
-        self.payments.append((i+1, payment, principal, interest, balance))
-
-def print_schedule(self):
-    print("Payment schedule:")
-    print("{:<10} {:<15} {:<15} {:<15} {:<15}".format("Month", "Payment", "Principal", "Interest", "Balance"))
-    for payment in self.payments:
-        print("{:<10} {:<15.2f} {:<15.2f} {:<15.2f} {:<15.2f}".format(*payment))
-
-loan = Loan(10000, 5, 5)
-loan.payment_schedule()
-
-
 def index():
+    return redirect(url_for('new_loan'))
 
-    return '''
+@app.route('/new_loan', methods=['GET', 'POST'])
+def new_loan():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        amount = request.form['amount']
+        loan = Loan(name=name, email=email, amount=amount)
+        db.session.add(loan)
+        db.session.commit()
+        return redirect(url_for('loan_submitted'))
+    return render_template('new_loan.html')
 
-        <form method="post" action="/loan">
+@app.route('/loan_submitted')
+def loan_submitted():
+    return render_template('loan_submitted.html')
 
-            <label for="amount">Loan amount:</label>
+@app.route('/past_loans')
+def past_loans():
+    loans = Loan.query.all()
+    return render_template('past_loans.html', loans=loans)
 
-            <input type="text" name="amount"><br>
-
-            <label for="interest_rate">Interest rate:</label>
-
-            <input type="text" name="interest_rate"><br>
-
-            <label for="term">Term in years:</label>
-
-            <input type="text" name="term"><br>
-
-            <input type="submit" value="Submit">
-
-        </form>
-
-    '''
-    
-from loan import Loan
-
-@app.route('/loan', methods=['POST'])
-
-def loan():
-
-    amount = float(request.form['amount'])
-
-    interest_rate = float(request.form['interest_rate'])
-
-    term = int(request.form['term'])
-
-    loan = Loan(amount, interest_rate, term)
-
-    loan.payment_schedule()
-
-    return '''
-
-        <h1>Payment schedule:</h1>
-
-        <table>
-
-            <thead>
-
-                <tr>
-
-                    <th>Month</th>
-
-                    <th>Payment</th>
-
-                    <th>Principal</th>
-
-                    <th>Interest</th>
-
-                    <th>Balance</th>
-
-                </tr>
-
-            </thead>
-
-            <tbody>
-
-                {% for payment in loan.payments %}
-
-                    <tr>
-
-                        <td>{{ payment[0] }}</td>
-
-                        <td>{{ payment[1] }}</td>
-
-                        <td>{{ payment[2] }}</td>
-
-                        <td>{{ payment[3] }}</td>
-
-                        <td>{{ payment[4] }}</td>
-
-                    </tr>
-
-                {% endfor %}
-
-            </tbody>
-
-        </table>
-
-    '''
- 
-loan.print_schedule()
-   
 if __name__ == '__main__':
-
-    app.run()
-    
+    app.run() 
